e822c5b09abab2631d23644724e17921ae336cfd
Francis Ribeiro <franedurib@gmail.com>
2017-04-04 10:22:12 -0300

    without bind

--- node-school-functional-javascript/partial_without_bind.js
+++ node-school-functional-javascript/partial_without_bind.js
@@ -1,2 +1,11 @@
 "use strict";
 
+var slice = Array.prototype.slice;
+
+var logger = function (namespace) {
+    return function () {
+        console.log.apply(console, [namespace].concat(slice.call(arguments, 0)));
+    }
+};
+
+module.exports = logger;
